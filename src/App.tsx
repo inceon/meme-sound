64dfa2c6ec4a53ebc26203275410ab3029c38ab1
inceon <mr.inceon@gmail.com>
2024-06-06 14:29:54 +0300

    nice meme

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import sound1 from './assets/sound/allo_cat.mp3'
 import sound2 from './assets/sound/da_eto_ya.mp3'
 import sound3 from './assets/sound/na_balkon.mp3'
 import sound4 from './assets/sound/misku_veliku.mp3'
+import sound5 from './assets/sound/nice.mp3'
 
 import image1 from './assets/image/allo_cat.jpeg'
 import image2 from './assets/image/da_eto_ya.jpeg'
 import image3 from './assets/image/na_balkon.jpeg'
 import image4 from './assets/image/miska_holubiv.jpeg'
+import image5 from './assets/image/nice.jpeg'
 
 function App() {
     const sounds = [
@@ -35,6 +37,12 @@ function App() {
             url: sound4,
             type: 'audio/mp3',
             image: image4
+        },
+        {
+            name: 'sound5',
+            url: sound5,
+            type: 'audio/mp3',
+            image: image5
         },
     ]
   return (<>
